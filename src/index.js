7ad86c10600d54588e160a904615e726f833712f
toann <toann@axon.com>
2020-08-08 23:06:46 +0700

    Create server & basic route API

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+
+import models from './models';
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+
+// hello world
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// getCategory()
+app.get('/categories', (req, res) => {
+  res.send(models.categories);
+});
+
+// getTopProduct()
+app.get('/topProducts', (req, res) => {
+  res.send(models.products);
+});
+
+// getProductByCategory()
+app.get('/products/:categoryId', (req, res) => {
+  res.send(models.products[req.params["categoryId"]]);
+});
+
+app.listen(PORT, () =>
+  console.log(`Express server currently running on port ${PORT}`)
+);
---
+++ src/models/index.js
@@ -0,0 +1,82 @@
+let categories = {
+  1: {
+    id: '1',
+    name: 'Giày dép'
+  },
+  2: {
+    id: '2',
+    name: 'Ba lô'
+  },
+  3: {
+    id: '3',
+    name: 'Lều trại'
+  },
+  4: {
+    id: '4',
+    name: 'Đồ điện tử'
+  },
+  5: {
+    id: '5',
+    name: 'Xe máy'
+  },
+  6: {
+    id: '6',
+    name: 'Dụng cụ nấu ăn'
+  },
+  7: {
+    id: '7',
+    name: 'Dụng cụ đa năng'
+  },
+  8: {
+    id: '8',
+    name: 'Các loại khác'
+  }
+};
+
+let products = {
+  1: {
+    id: '1',
+    category_id: '1',
+    name: 'Đôi giày',
+    image_url: 'https://p0.pikist.com/photos/276/90/shoes-tourism-dirt-camping-motion-travel-thumbnail.jpg',
+    price: '1,000,000',
+    owner_id: '1'
+  },
+  2: {
+    id: '2',
+    category_id: '2',
+    name: 'Ba lô TNF',
+    image_url: 'https://d2a2wjuuf1c30f.cloudfront.net/product_photos/65380347/file_c54337fa3f_original.jpg',
+    price: '200,000',
+    owner_id: '2'
+  },
+  3: {
+    id: '3',
+    category_id: '3',
+    name: 'Lều 4 người',
+    image_url: 'https://i.pinimg.com/originals/a3/1a/2b/a31a2bf1349dabd6d35bf3dc01ec247d.png',
+    price: '200,000',
+    owner_id: '3'
+  },
+  4: {
+    id: '4',
+    category_id: '4',
+    name: 'Thiết bị GPS',
+    image_url: 'https://www.beachcamera.com/shop/product-image.aspx?size=500&picId=513380',
+    price: '300,000',
+    owner_id: '4'
+  },
+  5: {
+    id: '5',
+    category_id: '5',
+    name: 'Xe win',
+    image_url: 'https://motorvina.com/wp-content/uploads/2016/10/honda-win-rent-sell-buy-hue-da-nang-hoi-an-motorvina-hanoi-motorbike-rental-hue-motorbike-rental-da-nang-motorbike-rental-ho-chi-minh-motorbike-rentalsemi-bike-rental-honda-win-.jpg',
+    price: '100,000',
+    owner_id: '5'
+  }
+};
+
+export default {
+  categories,
+  products,
+};
