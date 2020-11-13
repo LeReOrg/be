beb5ac65dc42a4cde94878425f9d1cf84bcbafb7
npdoan1996 <npdoan1996@gmail.com>
2020-11-12 17:38:58 -0800

    add connection to database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import MongoClient from "mongodb"; 
 
 import models from './models';
 import routes from './routes';
 
 const app = express();
 const PORT = 3000;
+const connectionString = 'mongodb+srv://npdoan1996:1npoyZqDpq6FK5VF@leaseandrent.k1rme.mongodb.net/<dbname>?retryWrites=true&w=majority'
 
 // Middleware
 app.use(cors());
@@ -20,6 +22,12 @@ app.use('/cart', routes.cart);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+MongoClient.connect(connectionString, {useUnifiedTopology: true})
+  .then(client => {
+    console.log('Connected to Database')
+  })
+  .catch(error => console.error(error))
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`Express server currently running on port ${PORT}`)
