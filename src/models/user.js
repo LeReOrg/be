f894337e069e3d122f9d6e2217c845d7c525f9a3
npdoan1996 <npdoan1996@gmail.com>
2020-12-26 16:23:27 -0800

    add GET user from db

---
+++ src/models/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"; 
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  id: { type: String, required: true},
+  order_id: { type: String, required: true},
+  first_name: { type: String, required: true},
+  last_name: { type: String, required: true},
+  gender: { type: String, required: true},
+  mobile: { type: String, required: true},
+  email: { type: String, required: true},
+  address_longitude: { type: Number, required: true},
+  address_latitude: { type: Number, required: true},
+}, {
+  timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,15 @@
 import { Router } from 'express'; 
 import models from '../models';
+import User from '../models/user';
 
 const router = Router(); 
+
+router.get('/', (req, res) => {
+    User.find()
+      .then(category => res.json(category))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  
 
 // getCartByUserID()
 router.get('/getCartByUserId/:userId', (req, res) => {
