5edb824647914b9e0af0e4f9dc5bf1667b9d5d4b
toann <toann@axon.com>
2021-01-11 20:40:23 +0700

    Pagenavigate Product

--- src/routes/product.js
+++ src/routes/product.js
@@ -7,10 +7,40 @@ const router = Router();
 // getTopProducts
 router.get('/getTopProduct', (req, res) => {
   Product.find()
-    .then(product => res.send(product))
-    .catch(err => res.status(400).json('Error: ' + err))
+    .then(product => res.status(200).json(product))
+    .catch(err => res.status(500).json('Error: ' + err))
 });
-  
+
+// getProductByPage
+router.get('/getProductByPage/:page', (req, res) => {
+  const pageOptions = {
+    page: parseInt(req.params["page"], 10) || 0,
+    limit: parseInt(req.params["limit"], 10) || 10
+  };
+
+  Product.find()
+    .skip(pageOptions.page * pageOptions.limit)
+    .limit(pageOptions.limit)
+    .exec(function (err, product) {
+      if (err) {
+        res.status(500).json(err);
+        return;
+      }
+      res.status(200).json(product);
+    });
+});
+
+// getProductPage
+router.get('/getProductPage', (req, res) => {
+  Product.countDocuments({}, function(err, count){
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
+    res.status(200).json(count);
+  });
+});
+
 // filter products by price: increase
 router.get('/filterByPrice/inc', (req, res) => {
     let sortable = [];
