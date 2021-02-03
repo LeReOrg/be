035452b50a6587864e504577b156b3a9e2c82bd2
toann <toann@axon.com>
2021-02-03 19:46:44 +0700

    Add FilterByLocation

--- src/routes/product.js
+++ src/routes/product.js
@@ -88,6 +88,68 @@ router.get('/getFirstImage/:Id', (req, res) => {
     .catch(err => res.status(500).json('Error: ' + err))
 })
 
+//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+function calcCrow(lat1, lon1, lat2, lon2)
+{
+  var R = 6371; // km
+  var dLat = toRad(lat2-lat1);
+  var dLon = toRad(lon2-lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c;
+  return d;
+}
+
+// Converts numeric degrees to radians
+function toRad(Value)
+{
+  return Value * Math.PI / 180;
+}
+
+// filterByLocation
+router.get('/filterByLocation', (req,res) => {
+  const pageOptions = {
+    page: parseInt(req.query["page"], 10) || 0,
+    limit: parseInt(req.query["limit"], 10) || 10
+  };
+
+  let page = 0;
+  Product.countDocuments({}, function(err, count){
+    if (!err) {
+      page = ~~(count / pageOptions.limit) + 1;
+    }
+  });
+
+  const lat = req.query.lat;
+  const long = req.query.long;
+
+  Product.find()
+    .sort({location: 1})
+    .skip(pageOptions.page * pageOptions.limit)
+      .limit(pageOptions.limit)
+      .populate('category')
+      .populate({ path: 'owner_id', select: 'first_name last_name email'})
+      .exec(function (err, product) {
+        if (err) {
+          res.status(500).json(err);
+          console.log("hhaa")
+
+          return;
+        }
+        let result = {};
+        let keyPage = 'numPage';
+        let keyProduct = 'products';
+        result[keyPage] = page;
+        result[keyProduct] = product;
+
+        res.status(200).json(result);
+      });
+});
+
 // addProduct
 router.post('/addProduct', (req,res) => {
   const id = req.body.id;
