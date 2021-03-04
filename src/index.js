e2cc28f09c6eebc6e490c3a6ac51c1ee752602df
toann <toann@axon.com>
2021-03-04 21:01:34 +0700

    Add cors

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Modular routes
 app.use('/category', routes.category);
 app.use('/product', routes.product);
